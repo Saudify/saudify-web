6380c76fe928eab0d10cfa93d227e4401faf9ccd
Janderson <jandersonmartins10@gmail.com>
2018-04-24 08:24:56 -0300

    Fix app.component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,37 @@
 import { TestBed, async } from '@angular/core/testing';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        LeafletModule.forRoot()
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+
+  it('should have api defined', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    const { markers, baseLayers, onMapReady } = fixture.componentInstance;
+    expect(markers).toBeTruthy();
+    expect(baseLayers).toBeTruthy();
+    expect(typeof onMapReady).toEqual('function');
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render section map tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    const actual = compiled.querySelector('section.map');
+    expect(actual).toBeTruthy();
   }));
 });

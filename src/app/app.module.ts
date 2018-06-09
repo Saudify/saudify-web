dfac4501b0caf5eed76e0fa768451d2b4d64d7e0
Janderson <jandersonmartins10@gmail.com>
2018-06-08 23:57:44 -0300

    PWA config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { FeatureCollectionModule } from './feature-collection/feature-collection.module';
 import { SharedModule } from './shared/shared.module';
@@ -16,7 +18,8 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     LeafletModule.forRoot(),
     SharedModule,
-    FeatureCollectionModule
+    FeatureCollectionModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
